55a78daf6560a67780c43b69b88c2cf76c589869
kirill <kirill@gmail.com>
2022-11-12 22:08:19 +0300

    dora

---
+++ Ex005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя ползователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "даша")
+{
+    Console.WriteLine("Урааа, это ДАША!!!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
