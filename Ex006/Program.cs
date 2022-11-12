d61345355c5eb202e2753c6b11579c3f1eb22d65
kirill <kirill@gmail.com>
2022-11-12 22:44:11 +0300

    max

---
+++ Ex006/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 4;
+int b = 2;
+int c = 6;
+int d = 8;
+int e = 1;
+
+int max = a;
+
+if( a > max ) max = a;
+if( b > max ) max = b;
+if( c > max ) max = c;
+if( d > max ) max = d;
+if( e > max ) max = e;
+
+Console.Write("max = ");
+Console.WriteLine(max);
